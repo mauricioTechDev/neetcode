5fdeea3ad2dbcec68001c7dae300de88959aef4b
Mauricio Acosta <mauricio.acosta92@gmail.com>
2023-07-25 21:37:57 -0400

    Update top-k-frequent-elements.js

--- roadmap/arraysAndHashing/top-k-frequent-elements.js
+++ roadmap/arraysAndHashing/top-k-frequent-elements.js
@@ -24,3 +24,41 @@ var topKFrequent = function(nums, k) {
 
     return res
 };
+
+// Improve to O(n)
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    let map = new Map();
+
+    for(let i = 0; i < nums.length; i+=1){
+        if(map.has(nums[i])){
+            let currentCount = map.get(nums[i])
+            map.set(nums[i], currentCount + 1)
+        } else {
+            map.set(nums[i], 1)
+        }
+    }
+
+    let bucketSort = new Array(nums.length + 1).fill(0);
+
+    map.forEach((value, key, map) => {
+        if(bucketSort[value] !== 0){
+            bucketSort[value].push(key)
+        } else {
+            bucketSort[value] = [key]
+        }
+    })
+
+    let ans = []
+    for(let j = bucketSort.length - 1; j >= 0; j-=1){
+
+        if(ans.length == k) return ans
+        if(bucketSort[j] !== 0){
+            ans = [...ans, ...bucketSort[j]];
+        }
+    }
+};
